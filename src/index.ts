03890c2753b51edfcdf10ffe548a6f34a7f50cec
solarvenom <contact@solarvenom.com>
2025-01-29 14:13:03 +0100

    add .env to run containers on specified variables

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
-const port: number = 3000;
+const port: number = Number(process.env.APP_PORT) || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+    res.send('Hello, World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
